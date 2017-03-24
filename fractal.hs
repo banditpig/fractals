5ac66f9c711b25954905767d6ab0f7d26d957544
mike-k-houghton <mike_k_houghton@yahoo.co.uk>
2017-03-24 15:48:10 +0000

    latest

--- fractal.hs
+++ fractal.hs
@@ -10,23 +10,30 @@ type Grid a = [[a]]
 
 w :: Int 
 w = 500
+
 h :: Int 
-h = 500
+h = 500 
 
---iterate f x == [x, f x, f (f x), ...]
-fofz :: (C -> C) -> C -> [C]
-fofz  = iterate  
+-- iterate f x == [x, f x, f (f x), ...]
+-- iterate :: (a -> a) -> a -> [a]
+fixC :: C
+fixC = (-1.037) :+ 0.17
 
-mandel :: C ->  [C]
-mandel   = fofz (\z -> z * z * z + 0.395)  ---0.1+0.651*i
+mandel :: C -> [C]
+mandel c = iterate (\z -> z * z + c)  (0.0 :+ 0.0)
+
+julia :: C ->  C -> [C]
+julia fixc c = iterate (\z -> z * z + fixc)  c
+
+julia' :: C -> [C]
+julia' = julia fixC
 
 close :: C -> Bool
-close z = magnitude z < 10
+close z = magnitude z < 5
 
---z3 + 0.400 - good
 chooseColor  :: [color] -> ( C -> [C]) -> C -> color
-chooseColor palette frac z = (palette !!) . length . take n . takeWhile close $ frac z
-  where n = length palette -  1
+chooseColor colors fz z = (colors !!) . length . take n . takeWhile close $ fz z
+  where n = length colors -  1
 
 grid :: Int -> Int -> Pnt -> Pnt -> Grid Pnt
 grid c r (xmin, ymin) (xmax, ymax ) = [[(x, y) | x <- for c xmin xmax ] | y <- for r ymin ymax ] where
@@ -35,34 +42,25 @@ grid c r (xmin, ymin) (xmax, ymax ) = [[(x, y) | x <- for c xmin xmax ] | y <- f
      where delta = (mx - mn) / fromIntegral (n - 1)
 
 grid1 :: Grid Pnt
-grid1 = grid w h (-2.0, -2.0) (2.0, 2.0)
+grid1 = grid w h (-1.5, -1.5) (1.5, 1.5)
 
-mapOverGrid :: [color] -> [[color]]
-mapOverGrid  colors = [ [chooseColor colors  mandel  (x :+ y) | (x,y) <- row] | row <- grid1 ]
+mapOverGrid :: (C -> [C]) -> [color] -> [[color]]
+mapOverGrid fz colors = [ [chooseColor colors  fz  (x :+ y) | (x,y) <- row] | row <- grid1 ]
 
 generateImg :: (Int -> Int -> PixelRGB8) -> DynamicImage
 generateImg gf = ImageRGB8 (generateImage gf w h)
 
-intColors :: [Int]
-intColors = [0..255]
-
-gridF :: [color] -> Int -> Int -> color
-gridF colors x y = ((mapOverGrid colors) !! y) !! x
-
-
-gridFunc :: Int -> Int -> PixelRGB8
-gridFunc x y = PixelRGB8 rgb rgb rgb where rgb = fromIntegral $ 255 -  ((mapOverGrid intColors) !! y) !! x
-
-gridFunc1 :: Int -> Int -> PixelRGB8
-gridFunc1 x y = ((mapOverGrid rgb8) !! y) !! x
+gridFunc :: (C -> [C]) -> Int -> Int -> PixelRGB8
+gridFunc fz  x y = ((mapOverGrid fz rgb8) !! y) !! x
 
 rgb8 :: [PixelRGB8]
-rgb8 = foldr (\x ac  -> PixelRGB8 x x x : ac) [] [255, 254..0]
+rgb8 = foldr (\x ac  -> PixelRGB8 x x x  : ac) [] [255, 254..0]
+
+rgb16 :: [PixelRGB16]
+rgb16 = foldr (\x ac  -> PixelRGB16 x x x : ac) [] [511, 510  ..0]
 
 
 main :: IO ()
-main = savePngImage "/Users/mikehoughton/haskell/book/image.png" $ generateImg  gridFunc1
 
-
-
+main = savePngImage "/Users/mikehoughton/haskell/book/image.png" $ generateImg . gridFunc $ mandel
 
